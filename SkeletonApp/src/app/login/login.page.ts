3b5310e22c2ae881f099e922d45beb071d375f40
Nicolas Munoz <munoz.mora.nicolas@gmail.com>
2024-08-14 17:11:52 -0400

    Login

--- SkeletonApp/src/app/login/login.page.ts
+++ SkeletonApp/src/app/login/login.page.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public progress = 0;
 
-  constructor() { }
+  constructor() { 
+    setInterval(() => {
+      this.progress += 0.01;
+
+      // Reset the progress bar when it reaches 100%
+      // to continuously show the demo
+      if (this.progress > 1) {
+        setTimeout(() => {
+          this.progress = 0;
+        }, 1000);
+      }
+    }, 50);
+  }
 
   ngOnInit() {
   }
