4e9b67ecfaa9695db4880d4d7dcc86a50aaa9e96
Nicolas Munoz <munoz.mora.nicolas@gmail.com>
2024-08-19 16:44:58 -0400

    Tour Heroes 5

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,20 +9,14 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent  implements OnInit {
 
-  selectedHero: Hero = {} as Hero;
   
   heroes : Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
