d7c99c26009d7f2c0bd55da697fd0efabcc912c9
Nicolas Munoz <munoz.mora.nicolas@gmail.com>
2024-08-19 10:53:25 -0400

    Init tour

--- SkeletonApp/src/app/login/login-routing.module.ts
+++ SkeletonApp/src/app/login/login-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginPage } from './login.page';
-import { DetailComponent } from '../detail/detail.component';
 
 const routes: Routes = [
   {
---
+++ tour-of-heroes/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tour-of-heroes',
+  webDir: 'www'
+};
+
+export default config;
