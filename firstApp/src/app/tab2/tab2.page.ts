40701e25a9efd0d8d8d0654d98b163ef4d6aefbf
Nicolas Munoz <munoz.mora.nicolas@gmail.com>
2024-10-16 12:58:41 -0300

    firstapp

--- firstApp/src/app/tab2/tab2.page.ts
+++ firstApp/src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto } from '../interfaces/user-photo';
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,35 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService,public actionSheetController: ActionSheetController) {}
 
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+  
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
